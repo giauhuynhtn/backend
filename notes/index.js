1457f9dea886f742c98c2c10d945273f916a3b8f
Giau Huynh <giau.huynh@tuni.fi>
2022-04-20 18:29:44 +0300

    mongodb

--- notes/index.js
+++ notes/index.js
@@ -1,40 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
+const { default: mongoose } = require('mongoose')
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
     console.log('Body:  ', request.body)
     console.log('---')
     next()
-}
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
 }
 
 app.use(requestLogger)
@@ -47,29 +24,29 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes=> {
+    res.json(notes)
+  })
+})
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content===undefined) {
+    return response.status(400).json({error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  })
+  note.save().then(savedNote=> {
+    console.log('note saved!');
+    response.json(savedNote)
+  })
+  
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -79,15 +56,10 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (req, res) => {
+  Note.findById(req.param.id).then(note => {
+    res.json(note)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -95,7 +67,6 @@ const unknownEndpoint = (request, response) => {
   }
   
 app.use(unknownEndpoint)
-
 
 const PORT = process.env.PORT || 3006
 app.listen(PORT, () => {
